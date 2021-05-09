b4ba5dc896fd943569a6b026de2375fafe8d28df
Noriyuki-Kobayashi <noriyuki.kobayashi@outlook.com>
2021-05-09 22:11:52 +0900

    プロジェクト ファイルを追加します。

---
+++ Animation Bound Ball/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Animation_Bound_Ball
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+
+            int speed = 120; //コマ間隔時間をmsec単位で指定します。
+            int i = 0;
+
+            do
+            {
+
+                Console.Clear();
+                Console.WriteLine("\n\n\n               (*^▽^)_");
+                Console.WriteLine("                       o\n\n");
+
+                System.Threading.Thread.Sleep(speed); //指定した時間だけスレッドを一時停止します。
+
+                Console.Clear();
+                Console.WriteLine("\n\n               (*^▽^)/o\n\n\n\n");
+                System.Threading.Thread.Sleep(speed); //指定した時間だけスレッドを一時停止します。
+
+                i++;
+
+            }
+            while (i <= 10);
+
+            Console.Clear();
+            Console.WriteLine("\n\n\n               ( ･＿･)_");
+            Console.WriteLine("                       x\n\n");
+            Console.ReadKey();
+
+        }
+    }
+}
